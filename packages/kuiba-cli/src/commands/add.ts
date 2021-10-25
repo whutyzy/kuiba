05611d2d2f2813260264df85ad2b786bc3fe6bae
whutyzy <whutyzy@foxmail.com>
2021-10-25 14:47:20 +0800

    完善add创建组件模板

--- packages/kuiba-cli/src/commands/add.ts
+++ packages/kuiba-cli/src/commands/add.ts
@@ -52,6 +52,7 @@ import { mount } from '@vue/test-utils'
 test('test ${name} example', () => {
   const wrapper = mount(example)
   expect(wrapper.html()).toMatchSnapshot()
+  wrapper.unmount()
 })
 
 test('test ${name} plugin', () => {
