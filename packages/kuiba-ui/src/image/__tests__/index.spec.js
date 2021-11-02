3d8feeac351b68b9f5e58bcabf1ab1ec26ee5008
whutyzy <whutyzy@foxmail.com>
2021-11-03 00:27:35 +0800

    添加lazyload单元测试

--- packages/kuiba-ui/src/image/__tests__/index.spec.js
+++ packages/kuiba-ui/src/image/__tests__/index.spec.js
@@ -5,14 +5,14 @@ import { mount } from '@vue/test-utils'
 
 const IMAGE_URL = 'https://i.loli.net/2021/11/01/jTC5EJK6UrZMkul.jpg'
 test('test image example', () => {
-  const wrapper = mount(example)
-  expect(wrapper.html()).toMatchSnapshot()
-  wrapper.unmount()
+    const wrapper = mount(example)
+    expect(wrapper.html()).toMatchSnapshot()
+    wrapper.unmount()
 })
 
 test('test image plugin', () => {
-  const app = createApp({}).use(Image)
-  expect(app.component(Image.name)).toBeTruthy()
+    const app = createApp({}).use(Image)
+    expect(app.component(Image.name)).toBeTruthy()
 })
 
 test('should emit load event after image loaded', () => {
@@ -26,9 +26,24 @@ test('should emit error event whenn load image failed ', () => {
     const wrapper = mount(Image, { props: { src: IMAGE_URL } })
     wrapper.find('img').trigger('error')
     expect(wrapper.emitted('error')[0][0]).toBeTruthy()
-     wrapper.unmount()
+    wrapper.unmount()
 })
+test('should emit onLoad & onEError in lazymode', () => {
+    const wrapper = mount(Image, { props: { src: IMAGE_URL, lazy: true } })
+    const onLoad = jest.fn()
+    const onError = jest.fn()
+    const img = wrapper.find('img')
+    img.element._lazy.state = 'success'
+    img.trigger('load')
+    expect(onLoad).toHaveBeenCalledTimes(1)
 
+    img.element._lazy.state = 'error'
+    img.trigger('error')
+    expect(onError).toHaveBeenCalledTimes(1)
+
+    wrapper.unmount()
+
+})
 test('test image fit', async () => {
     const wrapper = mount(Image)
 
@@ -48,5 +63,3 @@ test('test image fit', async () => {
     expect(wrapper.html()).toMatchSnapshot()
     wrapper.unmount()
 })
-
-
--- packages/kuiba-ui/src/lazyload/__tests__/index.spec.js
+++ packages/kuiba-ui/src/lazyload/__tests__/index.spec.js
@@ -3,13 +3,31 @@ import Lazyload from '../index'
 import { createApp } from 'vue'
 import { mount } from '@vue/test-utils'
 
+const Wrapper = {
+    directives: { Lazyload },
+    data: () => ({
+        src: 'https://images.pexels.com/photos/245035/pexels-photo-245035.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        error: 'https://images.pexels.com/photos/1458916/pexels-photo-1458916.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    }),
+    template: `
+    <img
+      :lazy-error="error"
+      v-lazyload="src"
+    >
+  `
+}
 test('test lazyload example', () => {
-  const wrapper = mount(example)
-  expect(wrapper.html()).toMatchSnapshot()
-  wrapper.unmount()
+    const wrapper = mount(example)
+    expect(wrapper.html()).toMatchSnapshot()
+    wrapper.unmount()
 })
 
 test('test lazyload plugin', () => {
-  const app = createApp({}).use(Lazyload)
-  expect(app.component(Lazyload.name)).toBeTruthy()
+    const app = createApp({}).use(Lazyload)
+    expect(app.component(Lazyload.name)).toBeTruthy()
 })
+
+test('should load when img in viewport', () => {})
+test('should attempt to load when  loaderror', () => {})
+
+test('should update img src when binding change', () => {})
